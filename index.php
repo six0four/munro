906f9ee22a5cb6f75dfd11644611df9f0c6d6721
six0four <kmedri@hotmail.com>
2016-10-24 12:40:00 -0400

    index content shifted back towards original intent

--- index.php
+++ index.php
@@ -1,52 +1,29 @@
 <!-- e-portfolio http://munro.humber.ca/~mdrk0011/index.php -->
-<?php include 'include/e-portfolioheader.php';?>
+<?php include 'include/prototypeheader.php';?>
 <DIV ID="Wrapper">
  <ARTICLE ID="Article_1">
   <HEADER ID="Header_Article_1">
-   <h2></h2>
+   <h2>Welcome to the <a href="http://humber.zone">Prototype Lab</a></h2>
   </HEADER>
   <SECTION ID="Section_1">
-   <H3>Prototype Lab</H3>
-   <P>
-	Kristian is very proud of all of the efforts of those involved 
-	with the <a href="http://humber.zone">Prototype Lab</a>. To find out
-	more about the projects that have been worked on, while the space
-	has been under his leadership, please visit:
-	<a href="http://munro.humber.ca/~mdrk0011/projects/">
-    http://munro.humber.ca/~mdrk0011/projects/</a>
-   </p>
+   <H3></H3>
    <p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/MW1G-nwe_cc" frameborder="0" allowfullscreen></iframe>
    </p>
-   <p>
-Student Comment: "This is a legit improvement."
-   </p>
   </SECTION>
   <SECTION ID="Section_2">
-   <H3>Biography</H3>
+   <H3>Projects</H3>
+   <p>
+    For ongoing projects visit: <a href="http://six0four.github.io/#Section_3">
+    http://six0four.github.io/#Section_3</a>
+   </p>
    <P>
-<iframe src="http://www.humber.ca/appliedtechnology/people/kristian-medri" width="560" height="1060">Humber 
-Humber College Institute of Technology & Advanced Learning
-Faculty Page</iframe>
-<a href="https://ca.linkedin.com/pub/kristian-medri/1/8a9/112">
-<img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_viewmy_160x33.png" width="160" height="33" border="0" alt="View Kristian Medri's profile on LinkedIn">
-</a> with a combined statment on <a href="https://www.linkedin.com/pulse/teaching-philosophy-kristian-medri">Teaching Philosophy, Professional Development, and Reflection</a>:
+    For past projects please visit: <a href="http://munro.humber.ca/~mdrk0011/projects/">
+    http://munro.humber.ca/~mdrk0011/projects/</a>
    </P>
-   <p>
-   There is a tremendous amount of freely available electronics and 
-computing information. The value that a teacher adds comprises of 
-delivering guidance, structure, and feedback to the student. I look 
-forward to continuous development, adaption, and tuning of these 
-skills.
-   </p>
-   <p>
-    <a href="http://www.researchgate.net/researcher/71802703_Kristian_E_Medri">ResearchGate 
-Profile
-	</a>
-   </p>
   </SECTION>
-  <SECTION ID="Section_7">
-   <H3>Twitter Feed</h3>
+  <SECTION ID="Section_3">
+   <H3>News Feed</h3>
    <p>
 <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Great showcase of support for teaching and learning excellence at the CTL launch party!  <a href="https://twitter.com/EileenDeCourcy">@EileenDeCourcy</a> <a href="http://t.co/aJwskOrjNZ">pic.twitter.com/aJwskOrjNZ</a></p>&mdash; Chris Whitaker (@humberpresident) <a href="https://twitter.com/humberpresident/status/426149022538006528">January 23, 2014</a></blockquote>
 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
@@ -70,106 +47,6 @@ Profile
 <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
   </p>
   </SECTION>
-    <SECTION ID="Section_8">
-   <H3>Ongoing Professional Development</h3>
-   <p>
-    <ul>
-<li>Participates in many on campus and off campus professional development opportunities including industry events and presenting at the League for Innovation STEMtech conference.</li>
-<li>CTL Git Session.</li>
-<li>Sign off plan for Teaching effectiveness program created.</li>
-<li>Took a Lynda.com quiz. Excursion to: Skills, The University of Waterloo 3D Print Centre which offers PCB (Printed Circuit Board) milling and manufacturing (https://uwaterloo.ca/3d-print-centre/circuit-board-manufacturing/pcb-technology) and the Velocity centre (http://velocity.uwaterloo.ca/)</li>
-<li>Campus wide Lynda.com subscription - recompleted a course via the institutional login, also as pointed out: https://openeducation.blackboard.com/ which will be interesting if it grows. As well as http://www.parl.gc.ca/Content/HOC/Committee/412/FINA/WebDoc/WD6615327/412_FINA_PBC2014_Briefs%5CDownsviewAerospaceInnovationAndResearch-e.pdf</li>
-<li>Signed up for https://www.edx.org/, Prototype Lab pie chart made in R.</li>
-<li>Created an xml document into which tagged points of interest can be entered, formatted by css, and parsed by javascript.</li>
-<li>Noticed: https://aws.amazon.com/iot/ (video has an arm similar to one built by part-time support staff) and http://docs.aws.amazon.com/iot/latest/developerguide/iot-device-sdk-node.html</li>
-<li>OPC seminar about industrial networks.</li>
-<li>Gender and Diversity training.</li>
-<li>Cisco VOIP Telephony system orientation was cancelled and the trainer was gone before I arrived (since I was late). 2 people registered.</li>
-<li>Writing MECH 106 exam and looking up equipment: Festo Diadactic Desktop CNC Mill Training System (Heavy Duty - for LabVolt this means can machine materials other than plastic) 5600-2 Festo Didactic Desktop CNC Lathe Training System (Heavy Duty - for LabVolt this means can machine materials other than plastic) with Automatic Tool Changer 5500-5 McMaster-Carr Drilling/Milling Machine 16" Manual Drill Feed, 1-1/4" Hole, 115/230V AC 8724A39 McMaster-Carr Vertical Band Saw for Metal, Wood, and Plastics 4145A12 McMaster-Carr Zoom Microscope with Digital Imaging 6.7X to 50X 22005A49 Desktop 3D printer Judco Manufacturing Inc Digi-Key Part Number 36-0100-00-ND http://media.digikey.com/pdf/Data%20Sheets/Judco%20PDFs/36-0100-00.pdf WELLER WFE2ESKIT1 ZERO SMOG VOLUME FUME EXTRACTION STATION KIT http://canada.newark.com/weller/ Oscilloscope the uses PC monitor as display using software and a peripheral via usb, Manufacturer BITSCOPE, Newark Part No.: 51Y5800 http://canada.newark.com/bitscope/bitscope-micro-bnc-port-adap/oscilloscope-adapter-2ch-20mhz/dp/51Y5800?ost=bitscope and categoryId=800000047003</li>
-<li>Discussion of having a Profinet Demo Platform Setup (as attached) at Humber came up during lunch at the seminar.</li>
-<li>Studying for and writing MECH 106 midterm, machining first piece.</li>
-<li>MECH106 class and lab as a registered student.</li>
-	</ul>
-   </p>
-  </SECTION>
-  <SECTION ID="Section_9">
-   <H3>Student Feedback/Comments</h3>
-   <p>
-    <ul>
-<li>The interest in the tutorial classes is nice. Labs are perfect. Everything's going very balanced in this course.</li>
-<li>Everything good.</li>
-<li>Very interesting course.</li>
-<li>Already good.</li>
-<li>Liked learning new methods and practicals.</li>
-<li>Sir you are my favourite teacher you are the best.</li>
-<li>Informative. Made blackboard website a good site to be active on.</li>
-<li>I liked the course.</li>
-<li>Our instructor is really good in his work, he always tries to give us excellent information.</li>
-<li>Liked learning new stuff.</li>
-<li>Liked everything. It is already best.</li>
-<li>I liked how the whole electronics concepts work and physics operates.</li>
-<li>It was fun. Nothing would make this course a better experience.</li>
-<li>Liked the way of learning.</li>
-<li>The labs were good and challenging.</li>
-<li>Really really good.</li>
-<li>Liked the practical stuff, fast phase to work. Extend the hours.</li>
-<li>Teacher was extremely knowledgeable and helpful.</li>
-<li>Many stuff and project ideas to work on. Nothing would make this course a better learning experience.</li>
-<li>Treated us as employees with professionalism.</li>
-<li>Very clear teaching.</li>
-<li>Amazing teacher.</li>
-<li>Prof. very helpful and fair. 5 stars.</li>
-<li>S'all good.</li>
-<li>Tutorials helped me prepare for the test. Kristian was the best professor for Tech 101, hopefully I have him for Tech 150, or Tech 152. He is helpful and better than other professors.</li>
-<li>What would make this course a better learning experience? N/A.</li>
-<li>I liked the fact that I learned a lot and that the course was very enjoyable and interesting.</li>
-<li>Its an interesting course. Iwant to explore new things about this course that's why I like the course. It also helps us to clear the concept of currents, voltages and resistances.</li>
-<li>I like the lab part of the course and numerical problem related to course. This course already provide better learning experience.</li>
-<li>Great professor, covers everything at least 3 times.</li>
-<li>He explained almost each step, he is helpful. He prepared us for tests very well, with quizzes and stuff. Also, he wants us to pass with a good mark.</li>
-<li>The correspondence of the labs with the lectures offered me better understanding.</li>
-<li>The course is extremely perfect. Usually, it forces every student to focus on its topics. In this course we did that things which are related to daily life. It really did a good job. Labs of this course are really useful.</li>
-<li>The course is very interesting and it is practical based course, so lab work gave me clear knowledge about each topic. Knowledge of this course is also very useful in real industry.</li>
-<li>I like the way he teaches me about the material.</li>
-<li>I like the way my performance improved.</li>
-<li>Kristian taught this course with a high degree of clarity and detail. I liked the fact that at no time in the course did I feel stressed due to a lack of understanding.</li>
-<li>I liked the lab preparation.</li>
-<li>I liked the way he taught simple and efficiently. I lie the way he helped prepare us for the test and his charisma.</li>
-<li>I think everything was excellent so just keep doing what your doing.</li>
-<li>Loved coming to his class, hands down my fav. teacher at Humber.</li>
-<li>I like the fact that he helps everyone who needs help.</li>
-<li>I like the way the professor teaches us, he always answers our concerns.</li>
-<li>I like the way the professor makes the tests and quizes.</li>
-<li>I liked solving circuits.</li>
-<li>Kristian is a very good instructor.</li>
-<li>I liked the Learning style and format.</li>
-<li>I liked the way he taught.</li>
-<li>I liked the fact how we had labs every week. It really helped my understanding of the topics.</li>
-<li>Everything is taught thoroughly.</li>
-<li>I liked the software programming part. Keep the course the same.</li>
-<li>I liked being treated more like an employee in the field.</li>
-<li>I love the part of soldering.</li>
-<li>I like building electronics.</li>
-<li>I like soldering, troubleshooting.</li>
-<li>The professor took the time to fully explain the material. No question was ever a stupid question.</li>
-<li>I like that it was varied and hands on.</li>
-<li>Soldering is FUN! :).</li>
-<li>Lectures are relevant to the labs.</li>
-<li>I liked learning new things such as soldering.</li>
-<li>I liked learning about different hardware boards, using Prototype Lab working with company.</li>
-<li>Gave us a good opportunity to integrate everything we've learned.</li>
-<li>Kristian is a great prof who understands what to teach to prepare us for the real world. The course is well outlined and expectations are set since the beginning.</li>
-<li>Professor did a good job and was able to help students.</li>
-<li>Best course I have taken in CENG so far.</li>
-<li>Pretty chill guy, chill is good.</li>
-<li>This course was really useful. Learnt a lot. The stuff was really helpful. The guidance of professor ws great. Excellent experience. Thank you!</li>
-<li>I liked hardware software integration and learning about different things.</li>
-<li>I liked group work, regular feedback.</li>
-<li>Fun.</li>
-<li>Hands Down the best teacher.</li>
-	</ul>
-   </p>
-  </SECTION>
  </ARTICLE>
 </DIV>
 <?php include 'include/switchcolumns.php';?>
@@ -179,23 +56,11 @@ Profile
                                 <LI><a 
 href="#Section_1">Prototype Lab</a></LI>
                                 <LI><a 
-href="#Section_2">Biography</a></LI>
+href="#Section_2">Projects</a></LI>
                                 <LI><a 
-href="#Section_2">Teaching Philosophy</a></LI>
-                                <LI><a 
-href="#Section_2">Professional Development Plan</a></LI>
-                                <LI><a 
-href="#Section_2">Reflections</a></LI>
-                                <LI><a 
-href="#Section_2">Research</a></LI>
-                                <LI><a 
-href="#Section_7">Twitter Feed</a></LI>
-                                <LI><a 
-href="#Section_8">Ongoing Professional Development</a></LI>
-                                <LI><a 
-href="#Section_9">Student Feedback/Comments</a></LI>
+href="#Section_3">News Feed</a></LI>
                         </UL>
                 </NAV>
 
-<?php include 'include/e-portfoliofooter.php';?>
+<?php include 'include/prototypefooter.php';?>
 
